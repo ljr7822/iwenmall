cfeb8600a1ad722f300e7e558cb94adc3db0e76e
iwenLjr <1816807688@qq.com>
2020-09-28 23:03:14 +0800

    统一接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+// 接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    window.location.href = '/#/login';
+  }else{
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios,axios);
 new Vue({
   router,
   render: h => h(App),
