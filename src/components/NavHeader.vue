43f5135fe7f9f3a9de3e682cfd58ec248b2d16e5
iwenLjr <1816807688@qq.com>
2020-09-30 16:04:04 +0800

    serviceBar组件

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -259,6 +259,7 @@ export default {
                             box-shadow:0px 7px 6px 0px rgba(0,0,0,0.11);
                             z-index: 10;
                             transition: height .5s;
+                            background-color: #ffffff;
                             .product{
                                position: relative;
                                float: left;
---
+++ src/components/ServiceBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="service">
+        <ul>
+            <li><span class="icon-setting"></span>预约维修服务</li>
+            <li><span class="icon-7day"></span>七天无理由退货</li>
+            <li><span class="icon-15day"></span>15天免费换货</li>
+            <li><span class="icon-post"></span>满150元包邮</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name:'service-bar'
+}
+</script>
+<style lang="scss">
+@import './../assets/sass/config.scss';
+@import './../assets/sass/mixin.scss';
+    .service{
+        padding: 33px 0;
+        color: $colorC;
+        font-size: $fontI;
+        li{
+            display: inline-block;
+            width: 24.9%;
+            text-align: center;
+            border-right: 1px solid $colorH;
+            span{
+                display: inline-block;
+                width: 20px;
+                height: 19px;
+                vertical-align: middle;
+                margin-right: 8px;
+            }
+            .icon-setting{
+                @include bgImg(20px,20px,'/imgs/icon-setting.png');
+            }
+            .icon-7day{
+                @include bgImg(20px,20px,'/imgs/icon-7day.png');
+            }
+            .icon-15day{
+                @include bgImg(20px,20px,'/imgs/icon-15day.png');
+            }
+            .icon-post{
+                @include bgImg(20px,20px,'/imgs/icon-post.png');
+            }
+        }
+    }
+</style>
