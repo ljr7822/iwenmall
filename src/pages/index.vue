c6ff10bb98c04ece00a950d21d13243c9832bf20
iwenLjr <1816807688@qq.com>
2020-10-02 19:49:25 +0800

    轮播图菜单显示-1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,96 +1,163 @@
 <template>
-    <div class="index">
-        <div class="container">
-            <div class="swiper-box">
-                <swiper ref="mySwiper" v-bind:options="swiperOption">
-                    <swiper-slide v-for="(item,index) in slideList" v-bind:key="index">
-                        <a v-bind:href="'/#/product/'+item.id"><img v-bind:src="item.img"></a>
-                    </swiper-slide>
-                    <!-- Optional controls -->
-                    <div class="swiper-pagination"  slot="pagination"></div>
-                    <div class="swiper-button-prev" slot="button-prev"></div>
-                    <div class="swiper-button-next" slot="button-next"></div>
-                </swiper>
-            </div>
-            <div class="ads-box"></div>
-            <div class="banner"></div>
-            <div class="product-box"></div>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 平板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插线板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能 路由器</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">生活 箱包</a>
+            </li>
+          </ul>
         </div>
-        <service-bar></service-bar>
+        <swiper ref="mySwiper" v-bind:options="swiperOption">
+          <swiper-slide v-for="(item, index) in slideList" v-bind:key="index">
+            <a v-bind:href="'/#/product/' + item.id"
+              ><img v-bind:src="item.img"
+            /></a>
+          </swiper-slide>
+          <!-- Optional controls -->
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
     </div>
+    <service-bar></service-bar>
+  </div>
 </template>
 <script>
-    import ServiceBar from './../components/ServiceBar';
-    import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
-    import 'swiper/css/swiper.css'
-    //import 'swiper/swiper-bundle.css'
-    // 要想有动画效果：swiper版本为5.4.5,如果默认安装则为6.x.x
-    // 修改步骤：
-    // 1.yarn remove swiper
-    // 2.yarn add swiper@5.4.5
-    // 3.import 'swiper/css/swiper.css'
-    export default {
-        name:'index',
-        components:{
-            Swiper,
-            SwiperSlide,
-            ServiceBar
+import ServiceBar from "./../components/ServiceBar";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+//import 'swiper/swiper-bundle.css'
+// 要想有动画效果：swiper版本为5.4.5,如果默认安装则为6.x.x
+// 修改步骤：
+// 1.yarn remove swiper
+// 2.yarn add swiper@5.4.5
+// 3.import 'swiper/css/swiper.css'
+export default {
+  name: "index",
+  components: {
+    Swiper,
+    SwiperSlide,
+    ServiceBar,
+  },
+  data() {
+    return {
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        effect: "cube",
+        cubeEffect: {
+          shadowOffset: 100,
+          shadowScale: 0.6,
         },
-        data(){
-            return{
-                swiperOption:{
-                    autoplay:true,
-                    loop:true,
-                    effect:'cube',
-                    cubeEffect: {
-                        shadowOffset: 100,
-                        shadowScale: 0.6
-                    },
-                    pagination: {
-                        el: '.swiper-pagination',
-                        clickable :true,
-                    },
-                    navigation: {
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg",
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg",
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg",
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-4.jpg",
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-5.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss">
+    @import './../assets/sass/config.scss';
+    @import './../assets/sass/mixin.scss';
+.index {
+  .swiper-box {
+    .nav-menu {
+        position:absolute;
+        width:264px;
+        height:451px;
+        z-index:9;
+        padding:26px 0;
+        background-color:#55585a7a;
+        box-sizing:border-box;
+        .menu-wrap{
+            .menu-item{
+                height:50px;
+                line-height:50px;
+                a{
+                    position: relative;
+                    display: block;
+                    font-size:$fontI;
+                    color:$colorG;
+                    padding-left: 30px;
+                    &:after{
+                        position: absolute;
+                        right: 30px;
+                        top: 17.5px;
+                        content: ' ';
+                        @include bgImg(10px,15px,'/imgs/icon-arrow.png');
                     }
-                },
-                slideList:[
-                    {
-                        id:'42',
-                        img:'/imgs/slider/slide-1.jpg'
-                    },
-                    {
-                        id:'45',
-                        img:'/imgs/slider/slide-2.jpg'
-                    },
-                    {
-                        id:'46',
-                        img:'/imgs/slider/slide-3.jpg'
-                    },
-                    {
-                        id:'',
-                        img:'/imgs/slider/slide-4.jpg'
-                    },
-                    {
-                        id:'',
-                        img:'/imgs/slider/slide-5.jpg'
-                    }
-                ]
+                }
+                &:hover{
+                    background-color: $colorA;
+                }
             }
         }
     }
-</script>
-<style lang="scss">
-    .index{
-        .swiper-box{
-            .swiper-container {
-                height: 451px;
-                img{
-                    width: 100%;
-                    height: 100%;
-                }
-            }  
-        }
+    .swiper-container {
+      height: 451px;
+      .swiper-button-prev{
+          left: 274px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
     }
+  }
+}
 </style>
