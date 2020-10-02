8f1c35fb4fd43d838fbe0943072dc04106621d6a
iwenLjr <1816807688@qq.com>
2020-10-03 02:42:54 +0800

    banner广告位完成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -40,7 +40,7 @@
             </li>
           </ul>
         </div>
-        <swiper ref="mySwiper" v-bind:options="swiperOption">
+        <swiper v-bind:options="swiperOption">
           <swiper-slide v-for="(item, index) in slideList" v-bind:key="index">
             <a v-bind:href="'/#/product/' + item.id"
               ><img v-bind:src="item.img"
@@ -52,8 +52,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a v-bind:href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+          <img v-bind:src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+          <a href="/#/product/30">
+            <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -139,6 +147,25 @@ export default {
             }
           ],
           [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+      ],
+
+      adsList:[
+          {
+              id:33,
+              img:'/imgs/ads/ads-1.png',
+          },
+          {
+              id:48,
+              img:'/imgs/ads/ads-2.jpg',
+          },
+          {
+              id:45,
+              img:'/imgs/ads/ads-3.png',
+          },
+          {
+              id:47,
+              img:'/imgs/ads/ads-4.jpg',
+          }
       ]
     };
   },
@@ -225,6 +252,19 @@ export default {
         height: 100%;
       }
     }
+  }
+  .ads-box{
+      @include flex();
+      margin-top: 14px;
+      margin-bottom: 31px;
+      a{
+          width: 296px;
+          height: 167px;
+      }
+  }
+  .banner{
+      margin-top: 30px;
+      margin-bottom: 50px;
   }
 }
 </style>
